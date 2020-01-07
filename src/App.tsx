1c114f98d0783e28ab68ec9699f310a3f364a037
kseniia.chuamchenko <kseniia.chumachenko@ataccama.com>
2020-01-07 15:08:27 +0200

    chore(CORS): proxy to work with diff origin added

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://william.multimediatech.cz:8081/air2day-test/api/graphql",
+    uri: "api/graphql",
     headers: {
       authorization: `fb4c1cd7-e219-48ef-be8f-5e31f125e64f`
     }
