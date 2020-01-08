574a660cb7a7dc298cfe4f32213f19a7330f2e54
kseniia.chuamchenko <kseniia.chumachenko@ataccama.com>
2020-01-08 18:14:23 +0200

    chore(deployment workflow): add env port to express server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const proxy = require("http-proxy-middleware");
 
+const PORT = process.env.PORT || 8080;
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
@@ -15,4 +17,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(8080);
+app.listen(PORT);
