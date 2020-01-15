cd66c7125085268afe06f1434641a60264dc9575
kseniia.chuamchenko <kseniia.chumachenko@ataccama.com>
2020-01-15 12:48:19 +0100

    feat(theming): theming WIP

--- src/theme.ts
+++ src/theme.ts
@@ -5,14 +5,37 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       main: blue[600],
-      light: blue[200]
+      light: blue[200],
+      dark: '',
+      contrastText: ''
     },
     secondary: {
       main: pink[600],
       light: pink[200]
     },
     error: {
-      main: red[600]
+      main: red[600],
+      light:'#e57373',
+      dark: '#d32f2f',
+      contrastText: ''
+    },
+    warning: {
+      main: '#ff9800',
+      light: '#ffb74d',
+      dark: '#f57c00',
+      contrastText: ''
+    },
+    info: {
+      main: '#2196f3',
+      light: '#64b5f6',
+      dark: '#1976d2',
+      contrastText: ''
+    },
+    success: {
+      main: '#4caf50',
+      light: '#81c784',
+      dark: '#388e3c',
+      contrastText: ''
     }
   },
   typography: {
